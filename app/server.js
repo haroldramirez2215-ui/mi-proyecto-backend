d70d948473e53822a2a8cc69f87087b1badada2b
harold <harold.ramirez.2215@gmail.com>
2026-07-05 14:50:27 -0500

    Agregar configuración de CORS

--- app/server.js
+++ app/server.js
@@ -1,18 +1,27 @@
 import express from "express";
+import cors from "cors";
+
 import clientesRoutes from "./routes/clientes.routes.js";
 import medicamentosRoutes from "./routes/medicamentos.routes.js";
 import movimientosRoutes from "./routes/movimientos.routes.js";
+
 const app = express();
 
+// Habilitar CORS
+app.use(cors());
+
+// Leer JSON
 app.use(express.json());
 
-// Aquí tienes tus rutas
+// Rutas
 app.use("/api/clientes", clientesRoutes);
 app.use("/api/medicamentos", medicamentosRoutes);
 app.use("/api/movimientos", movimientosRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     mensaje: "API PharmaTo-do funcionando correctamente"
   });
 });
+
 export default app;
