ba30013e41edb65155207736660557c8c1f5e034
harold <harold.ramirez.2215@gmail.com>
2026-07-05 15:25:01 -0500

    se agrega las importaciones de routes

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import app from './app/server.js';
 import config from './app/config/environments/index.js';
+import { Router } from 'express';
+import clientesRoutes     from './clientes.routes.js';
+import medicamentosRoutes from './medicamentos.routes.js';
+import movimientosRoutes  from './movimientos.routes.js';
+
+const router = Router();
+
+router.use('/clientes',     clientesRoutes);
+router.use('/medicamentos', medicamentosRoutes);
+router.use('/movimientos',  movimientosRoutes);
+
 
 const PORT = config.port;
 
