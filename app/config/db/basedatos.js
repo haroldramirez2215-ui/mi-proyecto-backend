7c9eca558334a08df5fbc87809f33fd45e69e345
harold <harold.ramirez.2215@gmail.com>
2026-07-05 15:41:11 -0500

    se agregan variables

--- app/config/db/basedatos.js
+++ app/config/db/basedatos.js
@@ -1,6 +1,8 @@
 import { createPool } from 'mysql2/promise';
 import config from '../environments/index.js';
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 // Pool de conexiones a MySQL
 export const pool = createPool({
   user:     config.db.user,
